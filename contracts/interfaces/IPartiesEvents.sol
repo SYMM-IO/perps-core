4471189afb7877b9012e6f71316d04b9db614872
rorschach <rorschach45001@gmail.com>
2025-11-25 16:21:16 +0100

    remove redundant functions

--- contracts/interfaces/IPartiesEvents.sol
+++ contracts/interfaces/IPartiesEvents.sol
@@ -64,14 +64,5 @@ interface IPartiesEvents {
 		QuoteStatus quoteStatus
 	);
 
-	event AdlClosePositions(
-		uint256 quoteId,
-		address partyA,
-		address partyB,
-		uint256 filledAmount,
-		uint256 closedPrice,
-		QuoteStatus quoteStatus
-	);
-
 	event LiquidatePartyB(address liquidator, address partyB, address partyA, uint256 partyBAllocatedBalance, int256 upnl);
 }
