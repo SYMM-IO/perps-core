e955b406252fa1dafa3b55787d593053d4bcec14
naveed <naveedinno@proton.me>
2025-12-15 08:59:51 +0100

    Fix test setup

--- test/Initialize.fixture.ts
+++ test/Initialize.fixture.ts
@@ -22,7 +22,7 @@ export async function initializeFixture(): Promise<RunContext> {
 	})
 
 	const instantLayer = await run("deploy:InstantLayer", {
-		symmioAddress: await diamond.getAddress(),
+		symmioaddress: await diamond.getAddress(),
 		admin: admin,
 	})
 
@@ -30,13 +30,13 @@ export async function initializeFixture(): Promise<RunContext> {
 
 	const affiliateHub: AffiliateHub = await run("deploy:affiliateHub", {
 		admin: context.signers.admin.address,
-		symmioFeeReceiver: context.signers.symmioFeeReceiver.address,
+		symmiofeereceiver: context.signers.symmioFeeReceiver.address,
 		logData: false,
 	})
 
 	const accountHub: AccountHub = await run("deploy:accountHub", {
 		admin: context.signers.admin.address,
-		affiliateHubAddress: await affiliateHub.getAddress(),
+		affiliatehubaddress: await affiliateHub.getAddress(),
 		logData: false,
 	})
 
