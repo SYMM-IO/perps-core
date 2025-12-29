1f921fd5b57c4fc74141563e8118360c30a5346c
droplet <tiny_ocean_droplet@gmail.com>
2025-12-29 09:02:20 +0100

    Adopting 8.5 imports so to be able to run tests

--- test/FeeDistributor.behavior.ts
+++ test/FeeDistributor.behavior.ts
@@ -1,7 +1,8 @@
 import {expect} from "chai"
 import {ethers, hre} from "./helpers/hardhat-connection.js"
 import type { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/types"
-import {MockSymmio, MockToken, SymmioFeeDistributor} from "../src/types/index.js"
+import type {MockSymmio, MockToken, SymmioFeeDistributor} from "../src/types/index.js"
+
 import {deployProxy} from "../utils/upgrades-shim.js"
 
 export function shouldBehaveLikeFeeDistributor() {
